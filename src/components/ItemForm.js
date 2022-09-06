ee3df93e8e2b45cb4e1ccde92e18d5d2200cdb02
shannone <shannone426@gmail.com>
2022-09-06 12:24:27 -0400

    alltests passing

--- src/components/ItemForm.js
+++ src/components/ItemForm.js
@@ -1,3 +1,58 @@
+// import React, { useState } from "react";
+
+// function ItemForm({ onAddItem }) {
+//   const [name, setName] = useState("");
+//   const [category, setCategory] = useState("Produce");
+
+//   function handleSubmit(e) {
+//     e.preventDefault();
+//     const itemData = {
+//       name: name,
+//       category: category,
+//       isInCart: false,
+//     };
+//     fetch("http://localhost:4000/items", {
+//       method: "POST",
+//       headers: {
+//         "Content-Type": "application/json",
+//       },
+//       body: JSON.stringify(itemData),
+//     })
+//       .then((resp) => resp.json())
+//       .then((newItem) => onAddItem(newItem));
+//   }
+
+//   return (
+//     <form className="NewItem" onSubmit={handleSubmit}>
+//       <label>
+//         Name:
+//         <input
+//           type="text"
+//           name="name"
+//           value={name}
+//           onChange={(e) => setName(e.target.value)}
+//         />
+//       </label>
+
+//       <label>
+//         Category:
+//         <select
+//           name="category"
+//           value={category}
+//           onChange={(e) => setCategory(e.target.value)}
+//         >
+//           <option value="Produce">Produce</option>
+//           <option value="Dairy">Dairy</option>
+//           <option value="Dessert">Dessert</option>
+//         </select>
+//       </label>
+
+//       <button type="submit">Add to List</button>
+//     </form>
+//   );
+// }
+
+// export default ItemForm;
 import React, { useState } from "react";
 
 function ItemForm({ onAddItem }) {
@@ -18,7 +73,7 @@ function ItemForm({ onAddItem }) {
       },
       body: JSON.stringify(itemData),
     })
-      .then((r) => r.json())
+      .then((resp) => resp.json())
       .then((newItem) => onAddItem(newItem));
   }
 
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,3 +1,4 @@
+
 import React, { useEffect, useState } from "react";
 import ItemForm from "./ItemForm";
 import Filter from "./Filter";
@@ -6,13 +7,32 @@ import Item from "./Item";
 function ShoppingList() {
   const [selectedCategory, setSelectedCategory] = useState("All");
   const [items, setItems] = useState([]);
-   
-  //displaying items
+
   useEffect(() => {
-    fetch(' http://localhost:4000/items')
-    .then((resp) => resp.json())
-    .then((items) => setItems(items)) //console.log(items)
-  },[])
+    fetch("http://localhost:4000/items")
+      .then((resp) => resp.json())
+      .then((items) => setItems(items));
+  }, []);
+
+  function handleDeleteItem(deletedItem) {
+    const updatedItems = items.filter((item) => item.id !== deletedItem.id);
+    setItems(updatedItems);
+  }
+
+  function handleUpdateItem(updatedItem) {
+    const updatedItems = items.map((item) => {
+      if (item.id === updatedItem.id) {
+        return updatedItem;
+      } else {
+        return item;
+      }
+    });
+    setItems(updatedItems);
+  }
+
+  function handleAddItem(newItem) {
+    setItems([...items, newItem]);
+  }
 
   function handleCategoryChange(category) {
     setSelectedCategory(category);
@@ -26,14 +46,19 @@ function ShoppingList() {
 
   return (
     <div className="ShoppingList">
-      <ItemForm />
+      <ItemForm onAddItem={handleAddItem} />
       <Filter
         category={selectedCategory}
         onCategoryChange={handleCategoryChange}
       />
       <ul className="Items">
         {itemsToDisplay.map((item) => (
-          <Item key={item.id} item={item} />
+          <Item
+            key={item.id}
+            item={item}
+            onUpdateItem={handleUpdateItem}
+            onDeleteItem={handleDeleteItem}
+          />
         ))}
       </ul>
     </div>
